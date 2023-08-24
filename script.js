d05a3ac62d5b8aaf63f4419e10b45c6adf931ab2
leticia-keller <143123662+leticia-keller@users.noreply.github.com>
2023-08-24 17:07:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function toggleMode() {
     img.setAttribute("src", "./assets1/avatar-lk-light.png")
   } else {
     // se tiver sem light mode, manter a imagem
-    img.setAttribute("src", "./assets1/avatar-lk-dark.png")
+    img.setAttribute("src", "./assets1/avatar_pb.jpeg")
   }
 }
